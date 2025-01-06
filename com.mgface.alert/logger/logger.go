282b7cb202d51fd7a304eaf541f777517324fab4
qazserena <qazserena@outlook.com>
2025-01-06 11:36:35 +0800

    新增常量

---
+++ com.mgface.alert/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"sync"
+)
+
+var (
+	logger *zap.Logger
+	once   sync.Once
+)
+
+// InitLogger 初始化日志管理器，输出到控制台
+func InitLogger(level zapcore.Level) {
+	once.Do(func() {
+		// 配置日志编码器
+		encoderConfig := zap.NewProductionEncoderConfig()
+		encoderConfig.TimeKey = "time"
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		encoderConfig.LevelKey = "level"
+		encoderConfig.MessageKey = "message"
+		encoderConfig.CallerKey = "caller"
+		encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+		encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+
+		// 创建日志核心，输出到控制台
+		core := zapcore.NewCore(
+			zapcore.NewConsoleEncoder(encoderConfig),                                // 控制台编码器
+			zapcore.Lock(zapcore.AddSync(zapcore.AddSync(zapcore.Lock(os.Stdout)))), // 输出到标准输出
+			level,                                                                   // 日志级别
+		)
+
+		// 创建全局 logger
+		logger = zap.New(core, zap.AddCaller())
+	})
+}
+
+// GetLogger 获取全局 logger
+func GetLogger() *zap.Logger {
+	if logger == nil {
+		panic("Logger is not initialized. Call InitLogger first.")
+	}
+	return logger
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mgface2022/mgface-alert-public
 go 1.23.4
 
 require (
+	go.uber.org/zap v1.27.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
 )
@@ -12,5 +13,6 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
